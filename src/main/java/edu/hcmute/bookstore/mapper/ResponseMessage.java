628c5abdc487cde7709b8eabe80b153e621f42fd
Mai Thanh Nhã <nhamt0306@gmail.com>
2022-11-08 22:54:58 +0700

    update response message

--- src/main/java/edu/hcmute/bookstore/mapper/ResponseMessage.java
+++ src/main/java/edu/hcmute/bookstore/mapper/ResponseMessage.java
@@ -2,9 +2,23 @@ package edu.hcmute.bookstore.mapper;
 
 public class ResponseMessage {
     private String message;
+    private String success;
 
     public ResponseMessage() {
 
+    }
+
+    public ResponseMessage(String message, String success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
     }
 
     public ResponseMessage(String message) {
