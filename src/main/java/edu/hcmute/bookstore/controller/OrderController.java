c3e2c784d75706aeeec8912f0e6d99b5f35c5e3d
Mai Thanh Nhã <nhamt0306@gmail.com>
2022-11-16 21:52:46 +0700

    gett all order

--- src/main/java/edu/hcmute/bookstore/controller/OrderController.java
+++ src/main/java/edu/hcmute/bookstore/controller/OrderController.java
@@ -92,6 +92,32 @@ public class OrderController {
             return new ResponseEntity<>("Cannot find any order!", HttpStatus.NOT_FOUND);
         }
     }
+    @GetMapping("/user/order/getAll")
+    public ResponseEntity<?> getOrderByUserLogin(){
+        try {
+            List<OrderEntity> orderEntityList = orderService.getAllByUserId(userDetailService.getCurrentUser().getId());
+            List<OrderMapper> orderMappers = new ArrayList<>();
+            for (OrderEntity orderEntity : orderEntityList)
+            {
+                OrderMapper orderMapper = new OrderMapper(orderEntity.getId(), orderEntity.getOrdTotalPrice(), orderEntity.getOrdNote(), orderEntity.getOrdShippingFee(), orderEntity.getOrdPayment(), orderEntity.getOrdStatus(), orderEntity.getOrdAddress(), orderEntity.getOrdPhone(), orderEntity.getCreate_at());
+                // get transaction of ~ order
+                List<TransactionMapper> transactionMappers = new ArrayList<>();
+                for(TransactionEntity transactionEntity : orderDetailService.getAllByOrderId(orderEntity.getId()))
+                {
+                    TransactionMapper transactionMapper = new TransactionMapper(transactionEntity.getId(),transactionEntity.getTranStatus(), transactionEntity.getTranUnitPrice(), transactionEntity.getTranQuantity(), transactionEntity.getProductEntity().getId(), transactionEntity.getProductEntity().getProImage(), transactionEntity.getProductEntity().getProName(), transactionEntity.getProductEntity().getAuthorEntity().getAutName());
+                    transactionMappers.add(transactionMapper);
+                }
+                orderMapper.setTransactionMapper(transactionMappers);
+                orderMappers.add(orderMapper);
+            }
+
+            return ResponseEntity.ok(orderMappers);
+        }
+        catch (Exception e)
+        {
+            return new ResponseEntity<>(LocalVariable.messageCannotFindCat , HttpStatus.NOT_FOUND);
+        }
+    }
 
     @PostMapping("/user/order/create")
     public Object createOrder(@RequestBody List<Object> req) throws ParseException {
