0725eee5a976d14dc03d1d0782399d7c4f582cbd
Mai Thanh Nhã <nhamt0306@gmail.com>
2022-12-01 00:17:51 +0700

    update recovery password method

--- src/main/java/edu/hcmute/bookstore/controller/PasswordRecoveryController.java
+++ src/main/java/edu/hcmute/bookstore/controller/PasswordRecoveryController.java
@@ -28,7 +28,7 @@ public class PasswordRecoveryController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @GetMapping(path = "/getOtp")
+    @PostMapping(path = "/getOtp")
     public OtpSendMailResponseDTO sendOtpRecoveryCodeToUserEmail(@RequestBody EmailRecoveryDTO email) {
         if(email.getEmail() == null) {
             throw new BadRequest("change password by address for user fail! need ?email= param");
@@ -57,7 +57,7 @@ public class PasswordRecoveryController {
 // sau khi người dùng xác thực otp thì check dưới db
 // Nếu otp đúng thì cho người dùng nhập password --> Call API recovery/{email} để đổi password;
 
-    @GetMapping(path = "/checkOtp")
+    @PostMapping(path = "/checkOtp")
     public Object changePasswordByOTP(@RequestBody RecoveryOTP recoveryOTP)
     {
         UserEntity user = userService.findByUserEmail(recoveryOTP.getEmail()).get();
